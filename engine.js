5e3c0ef847737d8d94994db1de5b2b7fc656cc8f
Angus Perkerson <angusp@angusp.com>
2018-01-21 23:16:34 -0800

    feat: Adds JIRA prompts
    Adds JIRA prompts for ticket number, status and comments

--- engine.js
+++ engine.js
@@ -1,41 +1,44 @@
-"format cjs";
+'format cjs';
 
 var wrap = require('word-wrap');
 var map = require('lodash.map');
 var longest = require('longest');
 var rightPad = require('right-pad');
 
-var filter = function(array) {
-  return array.filter(function(x) {
-    return x;
-  });
-};
+var filter = array => array.filter(x => !!x);
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
-module.exports = function (options) {
-
+module.exports = function(options) {
   var types = options.types;
-  var issueStatus = [{
-    name: 'No Change',
-    key: '',
-  }, {
-    name: 'To Do',
-    key: '#to-do',
-  }, {
-    name: 'In Progress',
-    key: '#in-progress',
-  }, {
-    name: 'In Testing',
-    key: '#in-testing',
-  }, {
-    name: 'Done',
-    key: '#done',
-  }];
+
+  // TODO: Refactor out to be a configurable file
+  var issueStatus = [
+    {
+      name: 'No Change',
+      value: ''
+    },
+    {
+      name: 'To Do',
+      value: '#to-do'
+    },
+    {
+      name: 'In Progress',
+      value: '#in-progress'
+    },
+    {
+      name: 'In Testing',
+      value: '#in-testing'
+    },
+    {
+      name: 'Done',
+      value: '#done'
+    }
+  ];
 
   var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function (type, key) {
+  var choices = map(types, function(type, key) {
     return {
       name: rightPad(key + ':', length) + ' ' + type.description,
       value: key
@@ -55,7 +58,9 @@ module.exports = function (options) {
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
     prompter: function(cz, commit) {
-      console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+      console.log(
+        '\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n'
+      );
 
       // Let's ask some questions of the user
       // so that we can populate our commit
@@ -64,92 +69,116 @@ module.exports = function (options) {
       // See inquirer.js docs for specifics.
       // You can also opt to use another input
       // collection library if you prefer.
-      cz.prompt([
-        {
-          type: 'list',
-          name: 'type',
-          message: 'Select the type of change that you\'re committing:',
-          choices: choices
-        }, {
-          type: 'input',
-          name: 'scope',
-          message: 'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n'
-        }, {
-          type: 'input',
-          name: 'subject',
-          message: 'Write a short, imperative tense description of the change:\n'
-        }, {
-          type: 'input',
-          name: 'body',
-          message: 'Provide a longer description of the change: (press enter to skip)\n'
-        }, {
-          type: 'confirm',
-          name: 'isBreaking',
-          message: 'Are there any breaking changes?',
-          default: false
-        }, {
-          type: 'input',
-          name: 'breaking',
-          message: 'Describe the breaking changes:\n',
-          when: function(answers) {
-            return answers.isBreaking;
+      cz
+        .prompt([
+          {
+            type: 'list',
+            name: 'type',
+            message: "Select the type of change that you're committing:",
+            choices: choices
+          },
+          {
+            type: 'input',
+            name: 'scope',
+            message:
+              'What is the scope of this change (e.g. component or file name)? (press enter to skip)\n'
+          },
+          {
+            type: 'input',
+            name: 'subject',
+            message:
+              'Write a short, imperative tense description of the change:\n'
+          },
+          {
+            type: 'input',
+            name: 'body',
+            message:
+              'Provide a longer description of the change: (press enter to skip)\n'
+          },
+          {
+            type: 'confirm',
+            name: 'isBreaking',
+            message: 'Are there any breaking changes?',
+            default: false
+          },
+          {
+            type: 'input',
+            name: 'breaking',
+            message: 'Describe the breaking changes:\n',
+            when: function(answers) {
+              return answers.isBreaking;
+            }
+          },
+          {
+            type: 'input',
+            name: 'issue',
+            message: 'What is the ticket for this commit? (e.g. JIRA-1234)'
+          },
+          {
+            type: 'list',
+            name: 'status',
+            message:
+              'What is the status of the ticket (e.g. In Progress, Done)? (press enter to skip)\n',
+            choices: issueStatus,
+            default: '',
+            when: function(answers) {
+              return !!answers.issue.trim();
+            }
+          },
+          {
+            type: 'input',
+            name: 'comment',
+            message:
+              'Is there a comment for the Jira Ticket? (Press enter to skip)\n',
+            when: function(answers) {
+              return !!answers.issue.trim();
+            }
           }
-        }, {
-          type: 'input',
-          name: 'issue',
-          message: 'What is the ticket for this commit? (e.g. JIRA-1234)',
-        }, {
-          type: 'list',
-          name: 'status',
-          message: 'What is the status of the ticket (e.g. In Progress, Done)? (press enter to skip)\n',
-          choices: issueStatus,
-          default: '',
-        }, {
-          type: 'input',
-          name: 'issueComment',
-          message: 'Is there a comment for the Jira Ticket? (Press enter to skip)\n',
-        }
-      ]).then(function(answers) {
+        ])
+        .then(function(answers) {
+          const MAX_LINE_WIDTH = 100;
 
-        const MAX_LINE_WIDTH = 100;
+          var wrapOptions = {
+            trim: true,
+            newline: '\n',
+            indent: '',
+            width: MAX_LINE_WIDTH
+          };
 
-        var wrapOptions = {
-          trim: true,
-          newline: '\n',
-          indent:'',
-          width: MAX_LINE_WIDTH,
-        };
+          // parentheses are only needed when a scope is present
+          var scope = answers.scope.trim();
+          scope = scope ? `(${answers.scope.trim()})` : '';
 
-        // parentheses are only needed when a scope is present
-        var scope = answers.scope.trim();
-        scope = scope ? `(${answers.scope.trim()})` : '';
+          // Hard limit this line
+          const subject = answers.subject.trim();
+          let head = `${answers.type}${scope}: ${answers.subject.trim()}`;
+          head = head.slice(0, MAX_LINE_WIDTH);
 
-        // Hard limit this line
-        const subject = answers.subject.trim()
-        let head = `${answers.type}${scope}: ${answers.subject.trim()}`;
-        head = head.slice(0, MAX_LINE_WIDTH);
+          // Wrap these lines at 100 characters
+          let body = wrap(answers.body, wrapOptions);
 
-        // Wrap these lines at 100 characters
-        let body = wrap(answers.body, wrapOptions);
+          // Apply breaking change prefix, removing it if already present
+          let breaking = answers.breaking ? answers.breaking.trim() : '';
+          breaking = breaking.replace(/^BREAKING CHANGE: /, '');
+          breaking = breaking ? `BREAKING CHANGE: ${breaking}` : '';
+          breaking = wrap(breaking, wrapOptions);
 
-        // Apply breaking change prefix, removing it if already present
-        let breaking = answers.breaking ? answers.breaking.trim() : '';
-        breaking = breaking.replace(/^BREAKING CHANGE: /, '');
-        breaking = breaking ? `BREAKING CHANGE: ${breaking}` : '';
-        breaking = wrap(breaking, wrapOptions);
+          let issues = answers.issue;
+          issues = answers.status ? `${issues} ${answers.status}` : issues;
 
-        let issues = answers.issues;
-        let comment = answers.issueComment ? answers.issueComment.trim().slice(0, MAX_LINE_WIDTH) : '';
+          let comment = answers.comment
+            ? answers.comment.trim().slice(0, MAX_LINE_WIDTH - issues.length)
+            : '';
+          issues = comment ? `${issues} #comment ${comment}` : issues;
 
-        issues = answers.status ? `${issues} ${answers.status}` : issues;
-        issues = comment ? `${issues} #comment ${comment}` : issues;
+          issues = issues ? wrap(issues, wrapOptions) : '';
 
-        issues = answers.issue ? wrap(answers.issues, wrapOptions) : '';
+          let commitMsg = `${head}\n\n${body}`;
+          commitMsg = breaking ? `${commitMsg}\n\n${breaking}` : commitMsg;
+          commitMsg = issues ? `${commitMsg}\n\n${issues}` : commitMsg;
 
-        let footer = filter([ breaking, issues ]).join('\n\n');
-
-        commit(`${head}\n\n${body}\n\n${footer}`);
-      });
+          commit(commitMsg);
+        });
     }
   };
 };
